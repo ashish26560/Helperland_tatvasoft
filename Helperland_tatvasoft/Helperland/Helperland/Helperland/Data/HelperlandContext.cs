6c6325a8ef82cefdab1d0b8e72b1f6e7c24b0693
Ashish Chauhan <ashish26560@gmail.com>
2022-01-26 11:52:22 +0530

    disabled the warning

--- Helperland_tatvasoft/Helperland/Helperland/Helperland/Data/HelperlandContext.cs
+++ Helperland_tatvasoft/Helperland/Helperland/Helperland/Data/HelperlandContext.cs
@@ -36,8 +36,10 @@ namespace Helperland.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
+#pragma warning disable CS1030 // #warning directive
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data source=LAPTOP-2I95FP4T;initial catalog=Helperland;Trusted_Connection=True;");
+#pragma warning restore CS1030 // #warning directive
             }
         }
 
