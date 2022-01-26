9b98c4cfbbf611419bd0cef353de1b9274b2f1cd
Ashish Chauhan <ashish26560@gmail.com>
2022-01-26 11:49:01 +0530

    Created a ASP.NET Core MVC application, with all public pages and linked them.

---
+++ Helperland_tatvasoft/Helperland/Helperland/Helperland/Controllers/PublicPagesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class PublicPagesController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Price()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
