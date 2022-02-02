d03f38d87daf8574185fd92326d57c063bda4af1
Ashish Chauhan <ashish26560@gmail.com>
2022-02-02 12:38:02 +0530

    added validation fields

--- Helperland_tatvasoft/Helperland/Helperland/Helperland/Models/ContactU.cs
+++ Helperland_tatvasoft/Helperland/Helperland/Helperland/Models/ContactU.cs
@@ -1,20 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 #nullable disable
 
 namespace Helperland.Models
 {
     public partial class ContactU
     {
+        //[Key]
         public int ContactUsId { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter email address")]
+        [Display(Name = "Email Address")]
+        //[EmailAddress]
         public string Email { get; set; }
+
+
         public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter phone number")]
+        [Display(Name = "Phone Number")]
+        [Phone]
         public string PhoneNumber { get; set; }
+
+
         public string Message { get; set; }
         public string UploadFileName { get; set; }
-        public DateTime? CreatedOn { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime? CreatedOn { get; set;
+           
+        }
         public int? CreatedBy { get; set; }
         public string FileName { get; set; }
     }
