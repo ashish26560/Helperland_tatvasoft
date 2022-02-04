8454db5098b49c46713e67ee43a98ac843cef15c
Ashish Chauhan <ashish26560@gmail.com>
2022-02-04 18:06:07 +0530

    added send mail to admin
    added logic for sending the mail to admin regarding any query

--- Helperland_tatvasoft/Helperland/Helperland/Helperland/Controllers/PublicPagesController.cs
+++ Helperland_tatvasoft/Helperland/Helperland/Helperland/Controllers/PublicPagesController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Helperland.Models;
 using Helperland.Data;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Helperland.Controllers
 {
@@ -24,29 +28,72 @@ namespace Helperland.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Contact(ContactU contactU,String Lastname )
+        public IActionResult Contact(ContactU contactU,String Lastname  )
         {
             if (ModelState.IsValid)
             {
-                using (HelperlandContext objHelperlandContext = new HelperlandContext())
+                using HelperlandContext objHelperlandContext = new HelperlandContext();
+          
+                string lastname = Convert.ToString(Lastname);
+                contactU.Name = (contactU.Name + " " + lastname);
+                contactU.Message = ("This mail is sent by " + contactU.Name + ", <br/><br/> Email:-  "+ contactU.Email+ "<br/> Phone number:-  " + contactU.PhoneNumber + "<br/><br/> Query:-  " +contactU.Message );
+
+                contactU.CreatedOn = DateTime.Now;
+               // contactU.FileName = Path.GetFileName(contactU.AttachmentFile.FileName);
+                //if (contactU.AttachmentFile != null)
+                //{
+                //    string folder = "~/media/";
+                //    folder += Guid.NewGuid().ToString() + "_" + contactU.AttachmentFile.FileName;
+                //    //string serverFolder = Path.Combine(IWebHostEnvironment.WebRootPath, folder);
+                //   // contactU.AttachmentFile.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
+                //    contactU.FileName = folder;
+
+                //}
+
+                objHelperlandContext.ContactUs.Add(contactU);
+                objHelperlandContext.SaveChanges();
+              
+                //sending mail logic starts here
+                using MailMessage mm = new MailMessage("ashish.chauhan93133@gmail.com", "ashishchauhan26560@gmail.com");
+
+                
+                //MailAddress by = new MailAddress("ashishchauhan26560@gmail.com");
+                //String to = new string("ashish26560@gmail.com");
+                
+
+                mm.Subject = contactU.Subject;
+                mm.Body = contactU.Message;
+                //mm.From = by;
+                //mm.To.Add(to);
+                //mm.To = to;
+
+                // mm.To = recepiant;
+
+                //if (contactU.AttachmentFile.Length > 0)
+                //{
+                //    string fileName = Path.GetFileName(contactU.AttachmentFile.FileName);
+                //    mm.Attachments.Add(new Attachment(contactU.AttachmentFile.OpenReadStream(), fileName));
+                //}
+
+                mm.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
                 {
-                    //ContactU contact = new ContactU
-                    //{
-                    //    CreatedOn = DateTime.Now
-                    //};
-                    //contactU.CreatedOn= contact.CreatedOn;
+                    String password = new string("FeelFree@2389");
+                    String from = new string("ashish.chauhan93133@gmail.com");
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred = new NetworkCredential(from, password);
 
-                    string lastname = Convert.ToString(Lastname);
-                    contactU.Name=(contactU.Name+" "+lastname);
-
-                    contactU.CreatedOn = DateTime.Now;
-
-                    objHelperlandContext.ContactUs.Add(contactU);
-                    objHelperlandContext.SaveChanges();
-                   // Int64 id = objEmployee.EmployeeID;
-                    ModelState.Clear();
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mm);
+                    ViewBag.Message = "Email has been sent to the admin";
                 }
-               // return View(objEmployee);
+
+
+                ModelState.Clear();
 
             }
             return View();
@@ -68,24 +115,22 @@ namespace Helperland.Controllers
         {
             if (ModelState.IsValid)
             {
-                using (HelperlandContext objHelperlandContext = new HelperlandContext())
-                {
-                    //ContactU contact = new ContactU
-                    //{
-                    //    CreatedOn = DateTime.Now
-                    //};
-                    //contactU.CreatedOn= contact.CreatedOn;
+                using HelperlandContext objHelperlandContext = new HelperlandContext();
+                //ContactU contact = new ContactU
+                //{
+                //    CreatedOn = DateTime.Now
+                //};
+                //contactU.CreatedOn= contact.CreatedOn;
 
 
-                    user.CreatedDate = DateTime.Now;
-                    user.ModifiedDate = DateTime.Now;
-                    user.UserTypeId = 1;
+                user.CreatedDate = DateTime.Now;
+                user.ModifiedDate = DateTime.Now;
+                user.UserTypeId = 1;
 
-                    objHelperlandContext.Users.Add(user);
-                    objHelperlandContext.SaveChanges();
-                    // Int64 id = objEmployee.EmployeeID;
-                    ModelState.Clear();
-                }
+                objHelperlandContext.Users.Add(user);
+                objHelperlandContext.SaveChanges();
+                // Int64 id = objEmployee.EmployeeID;
+                ModelState.Clear();
                 // return View(objEmployee);
 
             }
