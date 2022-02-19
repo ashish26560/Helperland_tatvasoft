29f80a8b0bb0bf8f8162f80f2577c69d462769d0
Ashish Chauhan <ashish26560@gmail.com>
2022-02-19 17:21:00 +0530

    added book now related methods
    CheckPostalCodemethod for the first tab,
    RequestService method for second tab,
    UserAddress method for getting user address in third tab, YourDetails method for updating address to the useraddress table, SendAddress method for sending the address selected by user, CompleteBooking method for sending all data at once to the database

--- Helperland_tatvasoft/Helperland/Helperland/Helperland/Controllers/PublicPagesController.cs
+++ Helperland_tatvasoft/Helperland/Helperland/Helperland/Controllers/PublicPagesController.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Helperland.Models;
 using Helperland.Data;
-using System.IO;
 using System.Net;
 using System.Net.Mail;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace Helperland.Controllers
 {
     public class PublicPagesController : Controller
     {
+        public readonly HelperlandContext _context;
+
+        public PublicPagesController(HelperlandContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult About()
         {
             return View();
@@ -26,6 +32,182 @@ namespace Helperland.Controllers
         {
             return View();
         }
+        public IActionResult Book_now()
+        {
+            return View();
+        }
+
+        public  IActionResult CheckPostalCode(User user)
+        {
+            var p = _context.Users.Where(c => c.ZipCode == user.ZipCode && c.UserTypeId == 2).FirstOrDefault();
+            if (p != null)
+            {
+                HttpContext.Session.SetString("ZipCode", p.ZipCode);
+
+                return View("Book_now");
+            }
+            else
+            {
+                //TempData["PostalCodeMessage"] = "Postal code you have entered is not valid.";
+
+                return StatusCode(404);
+
+                //return new HttpStatusCodeResult(401, "Custom Error Message 1");
+
+            }
+
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> YourDetails(UserAddress userAddress)
+        {
+            userAddress.UserId= (int)HttpContext.Session.GetInt32("UserId");
+            userAddress.IsDefault = false;
+            userAddress.IsDeleted = false;
+
+            await _context.UserAddresses.AddAsync(userAddress);
+            await _context.SaveChangesAsync();
+            return View("Book_now");
+        }
+
+        public IActionResult UserAddress()
+        {
+            int userid = (int)HttpContext.Session.GetInt32("UserId");
+            
+            var p = _context.UserAddresses.Where(c => c.UserId == userid).ToList();
+
+            return PartialView("UserAddress", p);
+        }
+
+
+        public async Task<IActionResult> CompleteBooking(ServiceRequest serviceRequest, ServiceRequestAddress serviceRequestAddress, ServiceRequestExtra serviceRequestExtra)
+        {
+
+            var data = HttpContext.Session.GetString("schedule");
+            serviceRequest = JsonConvert.DeserializeObject<ServiceRequest>(data);
+
+            var extraid = serviceRequest.Extra;
+            serviceRequest.ExtraHours=extraid * 0.5;
+            serviceRequest.ServiceHourlyRate=25;
+            var totalhours = (decimal)(serviceRequest.ExtraHours + serviceRequest.ServiceHours);
+            serviceRequest.SubTotal= (decimal)(totalhours * serviceRequest.ServiceHourlyRate);
+            serviceRequest.TotalCost= (decimal)(totalhours * serviceRequest.ServiceHourlyRate);
+            serviceRequest.PaymentDone = true;
+            serviceRequest.PaymentDue = false;
+            await _context.ServiceRequests.AddAsync(serviceRequest);
+            await _context.SaveChangesAsync();
+
+            var servicereqid = serviceRequest.ServiceRequestId;
+
+            //HttpContext.Session.SetInt32("ServiceRequestId", serviceRequest.ServiceRequestId);
+
+            HttpContext.Session.Remove("schedule");
+            
+            data = HttpContext.Session.GetString("serviceaddress");
+            serviceRequestAddress = JsonConvert.DeserializeObject<ServiceRequestAddress>(data);
+            serviceRequestAddress.ServiceRequestId = servicereqid;
+
+            //serviceRequestExtra.ServiceRequestId = (int)HttpContext.Session.GetInt32("ServiceRequestId");
+            await _context.ServiceRequestAddresses.AddAsync(serviceRequestAddress);
+            await _context.SaveChangesAsync();
+
+            HttpContext.Session.Remove("serviceaddress");
+
+            serviceRequestExtra.ServiceRequestId = servicereqid;
+            serviceRequestExtra.ServiceExtraId = extraid;
+
+            await _context.ServiceRequestExtras.AddAsync(serviceRequestExtra);
+            await _context.SaveChangesAsync();
+
+            return View("Book_now");
+        }
+
+        [HttpPost]
+        public IActionResult SendAddress(int radioValue, ServiceRequestAddress serviceRequestAddress)
+        {
+            //if (radioValue is null)
+            //{
+            //    throw new ArgumentNullException(nameof(radioValue));
+            //}
+
+            var p = _context.UserAddresses.Where(c => c.AddressId == radioValue).FirstOrDefault();
+
+            serviceRequestAddress.AddressLine1 = p.AddressLine1;
+            serviceRequestAddress.AddressLine2 = p.AddressLine2;
+            serviceRequestAddress.City = p.City;
+            serviceRequestAddress.Email = p.Email;
+            serviceRequestAddress.Mobile = p.Mobile;
+            serviceRequestAddress.PostalCode = p.PostalCode;
+
+
+
+
+            HttpContext.Session.SetString("serviceaddress", JsonConvert.SerializeObject(serviceRequestAddress));
+            //await _context.ServiceRequestAddresses.AddAsync(serviceRequestAddress);
+            //await _context.SaveChangesAsync();
+            return View("Book_now");
+        }
+        
+        [HttpPost]
+        public IActionResult RequestService(ServiceRequest booking )
+        {
+          
+            int extra = 0;
+            if (booking.ExtraService1)
+            {
+                extra++;
+            } 
+            if (booking.ExtraService2)
+            {
+                extra++;
+            } 
+            if (booking.ExtraService3)
+            {
+                extra++;
+            } 
+            if (booking.ExtraService4)
+            {
+                extra++;
+            } 
+            if (booking.ExtraService5)
+            {
+                extra++;
+            }
+
+            string date = booking.StartDate.ToString("yyyy-MM-dd");
+            string time = booking.StartTime.ToString("HH:mm:ss");
+            DateTime startDateTime = Convert.ToDateTime(date).Add(TimeSpan.Parse(time));
+
+
+            booking.ServiceStartDate = startDateTime;
+            booking.ZipCode = HttpContext.Session.GetString("ZipCode");
+            booking.UserId = (int)HttpContext.Session.GetInt32("UserId");
+            booking.CreatedDate = DateTime.Now;
+            booking.ModifiedDate = DateTime.Now;
+            booking.PaymentDone = true;
+            booking.PaymentDue = false;
+            booking.Extra = extra;
+
+            //await _context.ServiceRequests.AddAsync(booking);
+            //await _context.SaveChangesAsync();
+
+            //HttpContext.Session.SetInt32("ServiceRequestId", booking.ServiceRequestId);
+
+            //serviceRequestExtra.ServiceRequestId = (int)HttpContext.Session.GetInt32("ServiceRequestId");
+            //serviceRequestExtra.ServiceExtraId = extra;
+
+            //await _context.ServiceRequestExtras.AddAsync(serviceRequestExtra);
+            //await _context.SaveChangesAsync();
+
+            //TempData["ServiceMessagePos"] = "Your Service Request Has Successfully Been Admitted.";
+
+
+            //TempData["ServiceMessageNeg"] = "Entered Promocode is Invalid.";
+
+            HttpContext.Session.SetString("schedule", JsonConvert.SerializeObject(booking));
+            return View("Book_now");
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Contact(ContactU contactU,String Lastname  )
